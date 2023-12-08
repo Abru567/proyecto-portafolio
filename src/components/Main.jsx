1046250d1433d4f243415d324c059ae5487949a1
Abril Agustina Steinbach <abrilsteinbach@gmail.com>
2023-12-08 01:15:13 -0300

    a

--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -1,15 +1,25 @@
+import { fetchData } from ".//fetchData"
+import { Suspense } from 'react';
+import { productos } from './servicios/productos.json'
 import '../App.css'
-import { productos } from './servicios/productos'
+
+const apiData = fetchData(productos)
+
 export default function Main() {
-    const{data,loading}=productos;
-    return (
-     <div>{
-        <ul>
-         {loading && <li>loading...</li>}   
-        {data?.map((user)=> (<li key={user.main}>{user.main}</li>))}
-        {data?.map((user)=> (<li key={user.main}>{user.main}</li>))}
-        {data?.map((user)=> (<li key={user.main}>{user.main}</li>))}
-        </ul>
-        }</div>
+
+        const data = apiData.read()
+
+        return (
+
+                <div>
+                        <Suspense fallback={<div>loading...</div>}>
+                                <ul>
+                                        {data?.map((user) => { <li key={user.id}>{user.titulo}</li> })};
+                                        {data?.map((user) => { <li key={user.id}>{user.tipo}</li> })
+                                        }
+                                </ul>
+                        </Suspense>
+                </div>
+
         )
 }
