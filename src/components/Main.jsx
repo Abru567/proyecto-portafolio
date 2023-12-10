cfb4ddea998354848b2464313167a652e78daf1b
Abru567 <abrilsteinbach@gmail.com>
2023-12-10 15:08:17 -0300

    a

--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -3,13 +3,35 @@ import { Suspense } from 'react';
 import {Menu} from './servicios/app'
 import '../App.css'
 
-//const apiData = fetchData(menu)
 
 export default function Main() {
-        //const data = apiData.read()
+  const axios = require('axios');
+
+  // URL de tu API
+  const apiUrl = 'http://localhost:3000';
+  
+  // Obtener todo el menú
+  axios.get(`${apiUrl}/menu`)
+    .then(response => {
+      console.log('Menú:', response.data);
+    })
+    .catch(error => {
+      console.error('Error al obtener el menú:', error);
+    });
+  
+  // Obtener un elemento específico por ID (sustituye <id> con el ID deseado)
+  const itemId = 1;
+  axios.get(`${apiUrl}/menu/${itemId}`)
+    .then(response => {
+      console.log(`Elemento ${itemId}:`, response.data);
+    })
+    .catch(error => {
+      console.error(`Error al obtener el elemento ${itemId}:`, error);
+    });
+  
 
         return (
-                
+            <div>{axios}</div>
         ) //(
                 //<div>
                   //      <Suspense fallback={<div>loading...</div>}>
